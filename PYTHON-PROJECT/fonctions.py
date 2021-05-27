428369d0376c187acd7c17d1bb883d44a8b88abe
Frédérique WANDJI  K <frederique.wandji@ensta-bretagne.org>
2021-05-27 15:34:58 +0200

    ajout de fonctions retournant les caractéristiques d'un élement dont l'identifiant est donné

--- PYTHON-PROJECT/fonctions.py
+++ PYTHON-PROJECT/fonctions.py
@@ -24,20 +24,20 @@ class objets(object):
 def afficherCD_DVD():
         cursor.execute("""SELECT * FROM objets, CD_DVD where objets.idobjet=CD_DVD.idobjet""")
         liste = list(cursor)
-        print(liste)
+        return liste
 
 
 
 def afficherlivre():
     cursor.execute("""SELECT * FROM objets,livre where objets.idobjet=livre.idobjet""")
     liste = list(cursor)
-    print(liste)
+    return liste
 
 
 def afficherjournal():
     cursor.execute("""SELECT * FROM objets,journal where objets.idobjet=journal.idobjet""")
     liste = list(cursor)
-    print(liste)
+    return liste
 
 
 
@@ -45,7 +45,7 @@ def afficherjournal():
 def affichermagazine():
     cursor.execute("""SELECT * FROM objets,magazine where objets.idobjet=magazine.idobjet""")
     liste = list(cursor)
-    print(liste)
+    return liste
 
 
 def afficher():
@@ -53,6 +53,26 @@ def afficher():
     afficherlivre()
     afficherjournal()
     affichermagazine()
+
+def afficherIDC(identifiant):
+    cursor.execute(""" SELECT * FROM objets, CD_DVD where objets.idobjet=(?) AND objets.idobjet=CD_DVD.idobjet  """,(identifiant,))
+    liste=list(cursor)
+    return liste
+
+def afficherIDJ(identifiant):
+    cursor.execute(""" SELECT * FROM objets, journal where objets.idobjet=(?) AND objets.idobjet=journal.idobjet  """,(identifiant,))
+    liste=list(cursor)
+    return liste
+
+def afficherIDL(identifiant):
+    cursor.execute(""" SELECT * FROM objets, livre where objets.idobjet=(?) AND objets.idobjet=livre.idobjet  """,(identifiant,))
+    liste=list(cursor)
+    return liste
+
+def afficherIDM(identifiant):
+    cursor.execute(""" SELECT * FROM objets, magazine where objets.idobjet=(?) AND objets.idobjet=magazine.idobjet  """,(identifiant,))
+    liste=list(cursor)
+    return liste
 
 
 #obj1=CD_DVD(33322995654,"k","k","03-04-2020","03-04-2021","CD-DVD","1111","2:00:00","documentaire")
